7d0d334fc692b928b400bde56c70eb7b8fcb9379
Nova-techScience <prathameshs157@gmail.com>
2026-02-26 11:06:47 +0530

    contact page updated

--- app/contact/ContactClient.jsx
+++ app/contact/ContactClient.jsx
@@ -68,7 +68,7 @@ export default function ContactClient() {
           <p className="text-gray-600">{t.left.description}</p>
           <div className="flex items-center gap-3 mt-4">
             <Mail className="w-5 h-5 text-[#3386bc]" />
-            <span>novatechscience@gmail.com</span>
+            <span>info@novatechsciences.com</span>
           </div>
         </div>
 
